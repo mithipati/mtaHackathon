de21924dfca68c3504b52bee772c3465757e70aa
mithipati <neil.mithipati@gmail.com>
2015-01-27 22:30:29 -0800

    Comment out defaults

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,25 +23,25 @@
 
   // var navdrawerContainer = querySelector('.navdrawer-container');
   var body = document.body;
-  var appbarElement = querySelector('.app-bar');
-  var menuBtn = querySelector('.menu');
+  // var appbarElement = querySelector('.app-bar');
+  // var menuBtn = querySelector('.menu');
   var main = querySelector('main');
 
   function closeMenu() {
     body.classList.remove('open');
-    appbarElement.classList.remove('open');
+    // appbarElement.classList.remove('open');
     // navdrawerContainer.classList.remove('open');
   }
 
   function toggleMenu() {
     body.classList.toggle('open');
-    appbarElement.classList.toggle('open');
+    // appbarElement.classList.toggle('open');
     // navdrawerContainer.classList.toggle('open');
     // navdrawerContainer.classList.add('opened');
   }
 
   main.addEventListener('click', closeMenu);
-  menuBtn.addEventListener('click', toggleMenu);
+  // menuBtn.addEventListener('click', toggleMenu);
   // navdrawerContainer.addEventListener('click', function (event) {
     // if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
       // closeMenu();
